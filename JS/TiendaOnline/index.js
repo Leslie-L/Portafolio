e9258fb1f058ec3ade93b4d8953dfa65615a1a51
Leslie-L <leslie.centeno@galileo.edu>
2023-01-06 16:44:23 -0600

    menu mobil

--- JS/TiendaOnline/index.js
+++ JS/TiendaOnline/index.js
@@ -1,10 +1,16 @@
 const menuDesplegable = document.querySelector(".navbar-email");
 const menuDesplegableAbierto = document.querySelector(".desktop-menu");
-
+const menuMobil = document.querySelector("#menuMobil");
+const menuMobolAbierto = document.querySelector(".mobile-menu");
 
 menuDesplegable.addEventListener('click',showMenuDesplegable);
-
+menuMobil.addEventListener('click',showMenuMobil);
 
 function showMenuDesplegable() {
     menuDesplegableAbierto.classList.toggle("hide");
+
+}
+function showMenuMobil() {
+    menuMobolAbierto.classList.toggle("hide");
+
 }
