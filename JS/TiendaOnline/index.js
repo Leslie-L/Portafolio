257f5599d4f9feb2c8c0a89b24ee79835ee3ae21
Leslie-L <leslie.centeno@galileo.edu>
2023-01-10 21:19:11 -0600

    agregar al carrito

--- JS/TiendaOnline/index.js
+++ JS/TiendaOnline/index.js
@@ -6,6 +6,7 @@ const productDetalCart = document.querySelector(".navbar-shopping-cart");
 const productDetalCartAbierto = document.querySelector(".product-detail");
 const cardsContainer = document.querySelector('.cards-container');
 const modalDetailProduct = document.querySelector('.product-info-detail');
+const cantCart = document.querySelector('#cantCart');
 
 /*Modal detalles del producto */
 const buttonCloseModal = document.querySelector('#closeModal');
@@ -15,13 +16,31 @@ const titleModal = document.querySelector('#titleModal');
 const descriptionModal = document.querySelector('#descriptionModal');
 const buttonAddCart = document.querySelector('#buttonAddCart');
 
+const carrito = {
+
+}
 
 
 menuDesplegable.addEventListener('click',showMenuDesplegable);
 menuMobil.addEventListener('click',showMenuMobil);
 productDetalCart.addEventListener('click',showCart);
 buttonCloseModal.addEventListener('click',closeModal);
-
+function addCarrito(item) {
+    
+    const id = item['id'];
+    if(carrito[id]){
+        carrito[id] = carrito[id] + 1;
+    }else{
+        carrito[id] = 1;
+    }
+    cartNumber();
+}
+function cartNumber() {
+    const cantidad =  Object.values(carrito);
+    const total =  cantidad.reduce((acumulado,actual)=>acumulado+actual,0);
+    cantCart.textContent=total;
+    
+}
 
 function closeModal() {
     modalDetailProduct.classList.add('hide');
@@ -50,7 +69,10 @@ function showProd(event) {
     priceModal.textContent = '$' + item['price'];
     titleModal.textContent = item['title'];
     descriptionModal.textContent = item['description'];
-    
+    buttonAddCart.addEventListener('click',function() {
+        addCarrito(item);
+    });
+
 }
 async function datosProductos() {
     const response = await fetch('https://fakestoreapi.com/products');
@@ -78,8 +100,11 @@ async function datosProductos() {
         const figura = document.createElement('figure');
         const imgIcon = document.createElement('img');
         imgIcon.src="./icons/bt_add_to_cart.svg";
-        
         figura.appendChild(imgIcon);
+        figura.addEventListener('click',function() {
+            addCarrito(item);
+        });
+        figura.info=item;
 
         divInfoProd.appendChild(divAgrupador);
         divInfoProd.appendChild(figura);
@@ -90,6 +115,7 @@ async function datosProductos() {
         cardsContainer.appendChild(divPrincipal);
     }
 }
+cartNumber();
 datosProductos();
 
 /*
